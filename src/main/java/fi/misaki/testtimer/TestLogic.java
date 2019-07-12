0ea0f4d4729d6506065c5d345b3211e7dc622ea1
Ville Misaki <ville.misaki@rakuten.com>
2019-07-12 10:33:25 +0900

    Add histogram for verifying the fix, too.

--- src/main/java/fi/misaki/testtimer/TestLogic.java
+++ src/main/java/fi/misaki/testtimer/TestLogic.java
@@ -1,5 +1,6 @@
 package fi.misaki.testtimer;
 
+import org.eclipse.microprofile.metrics.Histogram;
 import org.eclipse.microprofile.metrics.MetricUnits;
 import org.eclipse.microprofile.metrics.Timer;
 import org.eclipse.microprofile.metrics.annotation.Metric;
@@ -14,8 +15,13 @@ public class TestLogic {
     @Metric(name = "TestTimer", unit = MetricUnits.MILLISECONDS, description = "test")
     private Timer testTimer;
 
+    @Inject
+    @Metric(name = "TestHistogram", unit = MetricUnits.MILLISECONDS, description = "test")
+    private Histogram testHistogram;
+
     public String getText() {
         testTimer.update(1234, TimeUnit.MILLISECONDS);
+        testHistogram.update(5678);
         return "Hello, world!";
     }
 }
